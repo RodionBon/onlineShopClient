193e2d609e42a7c5431c8c39789d3fc5d86cf784
Rodion <rodionb4649@gmail.com>
2025-10-14 14:40:40 +0300

    fix: fixed drawer icons size

--- app/components/appDrawer.tsx
+++ app/components/appDrawer.tsx
@@ -5,69 +5,54 @@ import { createTheme, Divider, Drawer, List, ListItem, ListItemButton, ListItemI
 import { deDE } from '@mui/material/locale';
 import { Link } from 'react-router';
 
-
-const drawerTheme = createTheme({
-    components: {
-        MuiSvgIcon: {
-            styleOverrides: {
-                root: {
-                    fontSize: 36,
-                }
-            }
-        }
-    }
-}, deDE);
-
 function AppDrawer({ isOpen, onClose }: { isOpen: boolean, onClose: DrawerProps['onClose'] }) {
     return (
-        <ThemeProvider theme={drawerTheme}>
-            <Drawer open={isOpen} onClose={onClose}>
-                <List sx={{ width: 400 }}>
-                    <ListItemButton component={Link} to="/productList">
-                        <ListItemIcon>
-                            <ListIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Warenliste" />
-                    </ListItemButton>
-                    <ListItemButton component={Link} to="/cart">
-                        <ListItemIcon>
-                            <ShoppingCartIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Warenkorb" />
-                    </ListItemButton>
-                    <ListItemButton component={Link} to="/account">
-                        <ListItemIcon>
-                            <PersonIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Mein Konto" />
-                    </ListItemButton>
-                    <Divider />
-                    <ListItem>
-                        <ListItemText primary="Aussehen" />
-                        <Select
-                            defaultValue="system"
-                            size="small"
-                            variant='standard'
-                        >
-                            <MenuItem value="light">Hell</MenuItem>
-                            <MenuItem value="dark">Dunkel</MenuItem>
-                            <MenuItem value="system">System</MenuItem>
-                        </Select>
-                    </ListItem>
-                    <ListItem >
-                        <ListItemText primary="Sprache" />
-                        <Select
-                            defaultValue="de"
-                            size="small"
-                            variant='standard'
-                        >
-                            <MenuItem value="de">Deutsch</MenuItem>
-                            <MenuItem value="ua">Ukrainisch</MenuItem>
-                        </Select>
-                    </ListItem>
-                </List>
-            </Drawer>
-        </ThemeProvider>
+        <Drawer open={isOpen} onClose={onClose}>
+            <List sx={{ width: 400 }}>
+                <ListItemButton component={Link} to="/productList">
+                    <ListItemIcon>
+                        <ListIcon sx={{ fontSize: 36 }} />
+                    </ListItemIcon>
+                    <ListItemText primary="Warenliste" />
+                </ListItemButton>
+                <ListItemButton component={Link} to="/cart">
+                    <ListItemIcon>
+                        <ShoppingCartIcon sx={{ fontSize: 36 }} />
+                    </ListItemIcon>
+                    <ListItemText primary="Warenkorb" />
+                </ListItemButton>
+                <ListItemButton component={Link} to="/account">
+                    <ListItemIcon>
+                        <PersonIcon sx={{ fontSize: 36 }} />
+                    </ListItemIcon>
+                    <ListItemText primary="Mein Konto" />
+                </ListItemButton>
+                <Divider />
+                <ListItem>
+                    <ListItemText primary="Aussehen" />
+                    <Select
+                        defaultValue="system"
+                        size="small"
+                        variant='standard'
+                    >
+                        <MenuItem value="light">Hell</MenuItem>
+                        <MenuItem value="dark">Dunkel</MenuItem>
+                        <MenuItem value="system">System</MenuItem>
+                    </Select>
+                </ListItem>
+                <ListItem >
+                    <ListItemText primary="Sprache" />
+                    <Select
+                        defaultValue="de"
+                        size="small"
+                        variant='standard'
+                    >
+                        <MenuItem value="de">Deutsch</MenuItem>
+                        <MenuItem value="ua">Ukrainisch</MenuItem>
+                    </Select>
+                </ListItem>
+            </List>
+        </Drawer>
     )
 }
 
